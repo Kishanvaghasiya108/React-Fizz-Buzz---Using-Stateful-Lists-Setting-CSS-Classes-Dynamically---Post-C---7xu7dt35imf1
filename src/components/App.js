a0c35d1f266fb863fd1a6bcac2225429f14b7b01
Kishanvaghasiya108 <122118895+Kishanvaghasiya108@users.noreply.github.com>
2023-02-26 21:00:00 +0530

    updated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useReducer } from "react";
 import "../styles/App.css";
+
 const App = () => {
-  const [count, setCount] = useState(1);
-  const [className, setClassName] = useState("normal");
-  useEffect(() => {
-    if (count % 15 === 0) {
-      setClassName("fizzbuzz");
-    } else if (count % 5 === 0) {
-      setClassName("buzz");
-    } else if (count % 3 === 0) {
-      setClassName("fizz");
-    } else {
-      setClassName("normal");
+  const initialState = 1;
+
+  const counterReducer = (state, action) => {
+    switch (action) {
+      case "increment":
+        return state + 1;
+      case "decrement":
+        return state - 1;
+      default:
+        return 0;
     }
-  }, [count]); // dependency array
-  // whenever count changes, useEffect should be called
+  };
+
+  const [state, disptch] = useReducer(counterReducer, initialState);
 
   return (
     <div id="main">
-      <button
-        id="increment"
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
+      <button id="increment" onClick={() => disptch("increment")}>
         Increment
       </button>
-      <div id="counter" className={className}>
-        {count}
-      </div>
-      <button
-        id="decrement"
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        Increment
+      <br />
+      <br />
+      {(state % 15 === 0 && (
+        <div id="counter" className="fizzbuzz">
+          {state}
+        </div>
+      )) ||
+        (state % 5 === 0 && (
+          <div id="counter" className="buzz">
+            {state}
+          </div>
+        )) ||
+        (state % 3 === 0 && (
+          <div id="counter" className="fizz">
+            {state}
+          </div>
+        )) ||
+        (state % 3 !== 0 && state % 5 !== 0 && state % 15 !== 0 && (
+          <div id="counter" className="normal">
+            {state}
+          </div>
+        ))}
+      <br />
+      <button id="decrement" onClick={() => disptch("decrement")}>
+        Decrement
       </button>
     </div>
   );
